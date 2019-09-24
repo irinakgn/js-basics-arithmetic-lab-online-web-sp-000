a548a5514907d6d1495b5d78ebd4ac80e76deff5
Irina Kagan <irinagamb@gmail.com>
2019-09-24 00:29:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,12 +9,9 @@ You should NOT try to redefine any of the above variables.
 // Write your code below this line!
 
 
+
 var newID
+var oldID
 var random = Math.random()
 
-if (currentAgeIsInteger) {
-  expect(ageIsValid).to.be.true;
-} else {
-  expect(ageIsValid).to.be.false;
-}
 Math.floor(Math.random() * 20) + 1;
