2287f49aa3318a9ec75d56d270f13981204b8d98
Irina Kagan <irinagamb@gmail.com>
2019-09-24 00:33:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ You should NOT try to redefine any of the above variables.
 
 
 var newID
-var oldID = 3
+var oldID = Math.random()
 
 //var random = Math.random()
 //Math.floor(Math.random() * 20) + 1;
