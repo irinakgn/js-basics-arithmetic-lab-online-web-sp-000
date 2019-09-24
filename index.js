7ec47956d608319852338490b237dcfe3b5f89d0
Irina Kagan <irinagamb@gmail.com>
2019-09-24 00:35:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ You should NOT try to redefine any of the above variables.
 
 
 
-var newID
-var oldID = Math.random()
 
+var oldID = Math.random()
+var newIDs = oldID + 1000000000
 //var random = Math.random()
 //Math.floor(Math.random() * 20) + 1;
